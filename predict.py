537b34e81f08ded969c45a2b824809a86b4136a0
devyaoyh <devyaoyh@gmail.com>
2020-03-01 10:58:46 -0600

    Positive detection of ch_74 anomaly

--- predict.py
+++ predict.py
@@ -6,10 +6,11 @@ from significance import get_test_z
 from significance import StatsAccum
 from scipy.stats import pearsonr
 
-WIN_SIZE = 500
-P_TEST = 0.01
+WIN_SIZE = 1000
+P_TEST = 0.0000001
 
-model = pickle.load(open('normed_mixed_model2.mdl','rb'))
+model = pickle.load(open('normed_mixed_model3.mdl','rb'))
+channel_norms = model['norms']
 channels = model['channels']
 channels_idx = {ch: i for i,ch in enumerate(channels)}
 num_channels = len(channels)
@@ -20,7 +21,10 @@ for ch in channels:
 
 # Start series prediction
 def run_trail():
-	avail_channels, channel_dat = draw_sample(data_path="validation",total_samples=1000)
+	avail_channels, channel_dat,_ = draw_sample(data_path="validation",total_samples=1000,norm=channel_norms)
+
+	# Normalize data
+
 	data_length = len(channel_dat[list(channel_dat.keys())[0]])
 
 	num_avail_channels = len(avail_channels)
@@ -46,17 +50,19 @@ def is_anomalous(predicted,Y,rho):
 	for i in range(min(WIN_SIZE,len(Y))):
 		corr_accum.add_point(predicted[i],Y[i])
 	p_val = corr_accum.calc_p(rho)
+	print(f"p: {p_val}")
 	if (p_val < P_TEST):
 		return p_val
 	for i in range(WIN_SIZE,len(Y)):
 		corr_accum.add_point(predicted[i],Y[i])
 		p_val = corr_accum.calc_p(rho)
+		print(f"p: {p_val}")
 		if (p_val < P_TEST):
 			return p_val
 	return -1
 
 def run_test(target_channels=None,dat_file=None):
-	avail_channels, channel_dat = extract_data(data_file="train\\COOLCAT_20100815_055813_69_20100815_055813_695.hdf" if dat_file is None else dat_file, max_len=1000)
+	avail_channels, channel_dat = extract_data(channel_norms,data_file="train\\COOLCAT_20100815_055813_69_20100815_055813_695.hdf" if dat_file is None else dat_file, max_len=1000)
 	# avail_channels, channel_dat = draw_sample(data_path="train",total_samples=10000)
 	data_length = len(channel_dat[list(channel_dat.keys())[0]])
 
@@ -66,18 +72,19 @@ def run_test(target_channels=None,dat_file=None):
 	err_ch = []
 
 	for ch in avail_channels if target_channels is None else target_channels:
-		if ch not in avail_channels:
+		if ch not in avail_channels or ch not in channels:
 			continue
 		X = np.asmatrix([channel_dat[c] if c != ch and c in channel_dat else np.zeros(data_length) for c in channels])
 		predicted = sensors[ch].predict(X).flatten()
 		Y = np.asarray(channel_dat[ch])
 		params = sensors[ch].lin_reg.coef_
 		coef_M.append(norm_zero(params))
-		expt_corr.append(pearsonr(Y,predicted)[0])
+		corr,_ = pearsonr(Y,predicted)
+		expt_corr.append(corr)
 		corr_exp = sensors[ch].get_exp_corr()
-		print(f"{ch} expected corr: {corr_exp}")
+		print(f"{ch} expected corr: {corr_exp} | Actual: {corr}")
 		err_p = is_anomalous(predicted,Y,corr_exp)
-		if (err_p > 0):
+		if (err_p >= 0):
 			err_ch.append(ch)
 			print(f"{ch} is Anomalous with {err_p}\% level of significance")
 		# ch_z_err = get_test_z(Y,predicted,sensors[ch].get_exp_corr())
@@ -88,19 +95,21 @@ def run_test(target_channels=None,dat_file=None):
 	if (target_channels is None):
 		return err_ch,expt_corr,coef_M,avail_channels
 	else:
-		ret_channels = [ch for ch in target_channels if ch in avail_channels]
+		ret_channels = [ch for ch in target_channels if ch in avail_channels and ch in channels]
 		return err_ch,expt_corr,coef_M,ret_channels
 
 # Validate
 print(f"Running correlation validation")
 run_trail()
-for ch in channels:
-	if (abs(sensors[ch].get_exp_corr()) > 0):
-		print(f"Sensor {ch} E[corr] = {sensors[ch].get_exp_corr():.5f}")
+# for ch in channels:
+# 	if (abs(sensors[ch].get_exp_corr()) > 0):
+# 		print(f"Sensor {ch} E[corr] = {sensors[ch].get_exp_corr():.5f}")
 
 # Test
 test_data_file = "train\\COOLCAT_20110805_194118_39_20110805_194118_391.hdf"
-test_channels = [f'ch_{i}' for i in range(70,111)]
+test_channels = None
+# test_channels = [f'ch_{i}' for i in range(70,111)]
+# test_channels = ['ch_18','ch_40','ch_74','ch_80','ch_90','ch_110']
 err_ch,expt_corr,coef_M,testing_channels = run_test(target_channels=test_channels,dat_file=test_data_file)
 print(err_ch)
 
